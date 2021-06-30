61548641acd24f1a240c184aff64ed28c96e4e71
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-06-30 09:01:48 +0700

    up code chang connect db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,13 @@ class DatabaseSeeder extends Seeder
         DB::table("admins")->insert([
             "name"=>"Administrator",
             "email"=>"admin@gmail.com",
+            "role"=>"ADMIN",
+            "password"=>bcrypt("12345678")
+        ]);
+        DB::table("admins")->insert([
+            "name"=>"User",
+            "email"=>"user@gmail.com",
+            "role"=>"USER",
             "password"=>bcrypt("12345678")
         ]);
     }
